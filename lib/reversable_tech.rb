e77cd38b9155921040506c9d73b64347f5a36096
Mikalai <mikolay.yankovsky@mail.ru>
2020-09-15 00:15:08 +0300

    hound fixes

--- lib/reversable_tech.rb
+++ lib/reversable_tech.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 module ReversableTech
   class Reverter
     AVAILABLE_ENTITIES = {
       String => [:reverse],
       Array => [:reverse],
-      Hash => [:to_a, :reverse, :to_h]
-    }
+      Hash => %i[to_a reverse to_h]
+    }.freeze
 
     def initialize(entity)
       @entity = entity
     end
 
     def call
-      AVAILABLE_ENTITIES[@entity.class].inject(@entity){|entity, method| entity.__send__(method) }
+      AVAILABLE_ENTITIES[@entity.class].inject(@entity) { |entity, method| entity.__send__(method) }
     end
   end
 end
