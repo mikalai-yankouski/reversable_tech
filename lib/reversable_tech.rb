1eb817e68da51eb0d8bf43afd1d1d4e60e5cea7b
Mikalai <mikolay.yankovsky@mail.ru>
2020-09-14 21:41:50 +0300

    add logic

--- lib/reversable_tech.rb
+++ lib/reversable_tech.rb
@@ -2,12 +2,18 @@ require "reversable_tech/version"
 
 module ReversableTech
   class Reverter
+    AVAILABLE_ENTITIES = {
+      String => [:reverse],
+      Array => [:reverse],
+      Hash => [:to_a, :reverse, :to_h]
+    }
+
     def initialize(entity)
       @entity = entity
     end
 
     def call
-
+      AVAILABLE_ENTITIES[@entity.class].inject(@entity){|entity, method| entity.__send__(method) }
     end
   end
 end
