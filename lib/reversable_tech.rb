aee2f215dda43921d36692bac9f5ba22a29512f7
Mikalai <mikolay.yankovsky@mail.ru>
2020-09-14 20:53:44 +0300

    init

---
+++ lib/reversable_tech.rb
@@ -0,0 +1,6 @@
+require "reversable_tech/version"
+
+module ReversableTech
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reversable_tech/version.rb
@@ -0,0 +1,3 @@
+module ReversableTech
+  VERSION = "0.1.0"
+end
